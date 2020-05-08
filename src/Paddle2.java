bb67e493471230de1aaf9db1afd424d3bb3400aa
rasmus.gillberg <xxyaymanyayxx@hotmail.com>
2020-05-08 11:23:50 +0200

    Ännu en bugg fixad, spelet crashade när paddle rörde sig längst ner på skärmen

--- src/Paddle2.java
+++ src/Paddle2.java
@@ -62,8 +62,8 @@ public class Paddle2 {
         if(boundingBox.y <= 0) {
             boundingBox.y = 0;
         }
-        if(boundingBox.y >= 280) {
-            boundingBox.y = 280;
+        if(boundingBox.y >= 240) {
+            boundingBox.y = 240;
         }
     }
 
